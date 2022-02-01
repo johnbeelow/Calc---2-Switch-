f36e2430befaa6fbc07626cef5da08c697c1767a
John <98343581+johnbeelow@users.noreply.github.com>
2022-02-01 15:18:29 +0300

    Add files via upload

---
+++ prom.js
@@ -0,0 +1,40 @@
+// ----------------------------------------------------------------CALC SWITCH VERSION -----------------------------------------------------------
+
+function Calc(func, a, b) {
+    // Делаем переменную что операнды а и b не являються числами
+    let isNotValid = (typeof a !== 'number') || (typeof b !== 'number');
+    // Запускаем проверку на то что а и b не являються числами, если true, запускается возврат 'Erorr'
+    if (isNotValid) {
+        return ('Error');
+    }
+    switch (func) {
+        case 'sum': 
+        return (a + b);
+        case 'sub': 
+        return (a - b);
+        case 'multi': 
+        return (a * b);
+        case 'div': 
+        return (a / b);
+        case 'rem': 
+        return (a % b);
+        case 'con': 
+        return(a ** b);
+        default:
+            return 'unknown operation';
+
+    }
+}
+
+console.log(Calc('sum', 40, 10));
+console.log(Calc('sub', 100, 50));
+console.log(Calc('multi', 5, 10));
+console.log(Calc('div', 100, 2));
+console.log(Calc('rem', 50, 100));
+console.log(Calc('con', 2, 6));
+console.log(Calc('non', 'non', 'non'));
+console.log(Calc('', '', ''));
+console.log(Calc());
+console.log(Calc('fff', 5, 5));
+
+
